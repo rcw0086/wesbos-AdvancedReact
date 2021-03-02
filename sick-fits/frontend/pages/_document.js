8f1c49636978085805f11f0b0b39a41be1f12f31
Robert Williams <rob@cratebind.com>
2021-03-02 08:30:29 -0600

    initial commit at the end of module 3

---
+++ sick-fits/frontend/pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, NextScript, Main} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  // server-rendered stylesheets and unique ideas in Next.js
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const styleTags = sheet.getStyleElement();
+    console.log(styleTags);
+    return { ...page, styleTags }
+  }
+  render () {
+    return (
+      <Html lang="en-CA">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+
+export default function SellPage() {
+  return (
+    <div>
+      <p>Hello!</p>
+    </div>
+  )
+}
+
+
+
+
